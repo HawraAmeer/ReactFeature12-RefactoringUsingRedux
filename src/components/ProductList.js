fb9676f888801b6a83a0c8f239dc3e405b2c32e3
HawraAmeer <hwra.ameer97@gmail.com>
2021-02-01 14:39:01 +0300

    11.Redux

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -4,21 +4,18 @@ import { ListWrapper } from "../styles";
 import ProductItem from "./ProductItem";
 import SearchBar from "./SearchBar";
 import { useState } from "react";
+import { useSelector } from "react-redux";
 
-const ProductList = ({ products, deleteProduct }) => {
+const ProductList = () => {
   const [query, setQuery] = useState("");
+
+  const products = useSelector((state) => state.products);
 
   const productList = products
     .filter((product) =>
       product.name.toLowerCase().includes(query.toLowerCase())
     )
-    .map((product) => (
-      <ProductItem
-        product={product}
-        key={product.id}
-        deleteProduct={deleteProduct}
-      />
-    ));
+    .map((product) => <ProductItem product={product} key={product.id} />);
 
   return (
     <div>
